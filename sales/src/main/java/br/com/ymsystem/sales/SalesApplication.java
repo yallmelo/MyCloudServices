4d055eb2bb70c9b1638a16fd1e759021d437e6eb
YallisoMelo <yallisonfelipe@outlook.com>
2019-06-04 15:04:56 -0300

    C.I

--- sales/src/main/java/br/com/ymsystem/sales/SalesApplication.java
+++ sales/src/main/java/br/com/ymsystem/sales/SalesApplication.java
@@ -1,7 +1,5 @@
 package br.com.ymsystem.sales;
 
-import br.com.ymsystem.sales.Repository.SaleRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
